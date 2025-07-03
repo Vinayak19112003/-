a0d13de496fd93d4bdc8b4d93eedd2841e8dcca9
Vinayak Deshmuk <vinayakdeshmuk191120033@gmail.com>
2025-07-03 05:13:30 +0000

    again in the firebase studio is correct but brouser have big gaps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -149,14 +149,14 @@ function Dashboard() {
                 <Skeleton className="h-28" />
             </div>
             <div className="grid grid-cols-1 lg:grid-cols-7 gap-4 md:gap-8">
-                <div className="lg:col-span-5"><Skeleton className="h-[450px]" /></div>
-                <div className="lg:col-span-2 space-y-4 md:space-y-8">
+                <div className="lg:col-span-4"><Skeleton className="h-[450px]" /></div>
+                <div className="lg:col-span-3 space-y-4 md:space-y-8">
                   <Skeleton className="h-[250px]" />
                   <Skeleton className="h-[250px]" />
                   <Skeleton className="h-[250px]" />
                 </div>
             </div>
-            <div className="full-width-chart">
+            <div>
                 <Skeleton className="h-[400px]" />
             </div>
             <Skeleton className="h-[400px]" />
@@ -234,10 +234,10 @@ function Dashboard() {
         <StatsCards trades={filteredTrades} />
         
         <div className="grid grid-cols-1 lg:grid-cols-7 gap-4 md:gap-8">
-            <div className="lg:col-span-5">
+            <div className="lg:col-span-4">
                 <MonthlyCalendar trades={trades} onDateSelect={handleCalendarDateSelect} />
             </div>
-            <div className="lg:col-span-2 flex flex-col gap-4 md:gap-8">
+            <div className="lg:col-span-3 flex flex-col gap-4 md:gap-8">
                 <Card>
                     <CardHeader>
                         <CardTitle>Performance Metrics</CardTitle>
@@ -274,9 +274,7 @@ function Dashboard() {
             </div>
         </div>
 
-        <div className="full-width-chart">
-          <EquityCurveChart trades={filteredTrades} />
-        </div>
+        <EquityCurveChart trades={filteredTrades} />
 
         <Card>
             <CardHeader className="flex flex-col sm:flex-row sm:items-center gap-2">
