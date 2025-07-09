89a2014f70f9d0aa78ee6955f644b918b15bc3d5
Vinayak Deshmuk <vinayakdeshmuk191120033@gmail.com>
2025-07-09 17:58:48 +0000

    if i click the button the url is change you see in the image left bottom

--- src/components/shell/sidebar.tsx
+++ src/components/shell/sidebar.tsx
@@ -59,16 +59,15 @@ export function Sidebar() {
         <SidebarMenu className="flex-1">
           {navItems.map((item) => (
             <SidebarMenuItem key={item.href}>
-              <SidebarMenuButton
-                asChild
-                isActive={isActive(item.href)}
-                tooltip={{ children: item.tooltip, side: 'right' }}
-              >
-                <Link href={item.href} prefetch={false}>
+              <Link href={item.href} prefetch={false} passHref asChild>
+                <SidebarMenuButton
+                  isActive={isActive(item.href)}
+                  tooltip={{ children: item.tooltip, side: 'right' }}
+                >
                   <item.icon />
                   <span>{item.label}</span>
-                </Link>
-              </SidebarMenuButton>
+                </SidebarMenuButton>
+              </Link>
             </SidebarMenuItem>
           ))}
           <SidebarMenuItem>
